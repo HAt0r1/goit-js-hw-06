a7d98c68c400bc1111ed493cd0b031b123cae72a
Hatori <ahgangadm@gmail.com>
2024-01-14 13:39:58 +0200

    Add solution for task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+class Storage {
+  #item;
+  constructor(item) {
+    this.#item = item;
+  }
+  getItems() {
+    return this.#item;
+  }
+  addItem(newItem) {
+    this.#item.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const indexItem = this.#item.indexOf(itemToRemove);
+    this.#item.splice(indexItem, 1);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
