a7ffcb91070f729a48eea67c9dbff5ad030c06dc
Hatori <ahgangadm@gmail.com>
2024-01-17 17:24:33 +0200

    Add 2 solutions for task-3.js

---
+++ js/task-3.js
@@ -0,0 +1,77 @@
+// // firts approach
+
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padStart(str) {
+    if (str) {
+      this.#value = str.concat(this.#value);
+    }
+  }
+  padEnd(str) {
+    if (str) {
+      this.#value = this.#value.concat(str);
+    }
+  }
+  padBoth(str) {
+    if (str) {
+      this.#value = str.concat(this.#value, str);
+    }
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+// second approach (my own method)
+// class StringBuilder {
+//   #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+//   padEnd(str) {
+//     if (str) {
+//       this.#value = [...this.#value, ...str];
+//     }
+//   }
+//   padStart(str) {
+//     if (str) {
+//       this.#value = [...str, ...this.#value];
+//     }
+//   }
+//   padBoth(str) {
+//     if (str) {
+//       this.#value = [...str, ...this.#value, ...str];
+//     }
+//   }
+
+//   returnToString() {
+//     return this.#value.join("");
+//   }
+// }
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+// builder.returnToString();
+// console.log(builder.returnToString());
